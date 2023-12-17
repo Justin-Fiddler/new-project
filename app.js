142ff978d7be95c371a53585c67de3287ef5ea7b
JUSTIN FIDDLER <JUSTIN.R.FIDDLER@GMAIL.COM>
2023-12-16 19:59:32 -0600

    adding JavaScript file

---
+++ app.js
@@ -0,0 +1,20 @@
+'use strict';
+alert("welcome to the 90s");
+alert("Throw on a flannel shirt, part your hair down the middle and let's dive in!")
+const bandName =prompt("What is your favorite band from the 90s?");
+console.log(bandName);
+
+document.write("Hi! " + bandName + " is one of our favorites too!");
+
+// if (this is true) {execute this code}
+
+if (bandName == "Nirvana"){
+    alert("Come as you are!");
+}   else if (bandName == "Radiohead"){
+    alert("Big fans of Kid A here!");
+}   else {
+    alert("Great answer");
+}
+
+
+
