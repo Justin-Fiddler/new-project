5710b853e12e3061aa2787b7e01f74cea7937de7
JUSTIN FIDDLER <JUSTIN.R.FIDDLER@GMAIL.COM>
2023-12-21 21:57:03 -0600

    Updated website with new java code

--- app.js
+++ app.js
@@ -16,42 +16,56 @@ function greetUser(){
     document.write("Hi! " + theirBand + " is one of our favorites too!");
 }
 
-function coolOrNo(){
-    const movieFan = prompt("Are the best movies from the 90s?")
-console.log(movieFan);
+// function coolOrNo(){
+//     const movieFan = prompt("Are the best movies from the 90s?")
+// console.log(movieFan);
 
-    if (movieFan == "yes"){
-        document.write('You are in the right place!');
-    }   else if (movieFan == "no"){
-        document.write('You have lots to learn here!');
-    }   else {
-        coolOrNoWarn();
-    }
+//     if (movieFan == "yes"){
+//         document.write('You are in the right place!');
+//     }   else if (movieFan == "no"){
+//         document.write('You have lots to learn here!');
+//     }   else {
+//         coolOrNoWarn();
+//     }
+// }
+
+// function coolOrNoWarn(){
+//     const movieFan2 = prompt("One more chance to be a good sport! Are the best movies from the 90s?")
+// console.log(movieFan2);
+
+//     if (movieFan2 == "yes"){
+//         document.write('You are in the right place!');
+//     }   else if (movieFan2 == "no"){
+//         document.write('You have lots to learn here!');
+//     }   else {
+//         coolOrNoWarn2();
+//     }
+// }
+
+// function coolOrNoWarn2(){
+//     const movieFan3 = prompt("I can do this all day pal! Are the best movies from the 90s? Come on!")
+// console.log(movieFan3);
+
+//     if (movieFan3 == "yes"){
+//         document.write('Finally, we agree on something!');
+//     }   else if (movieFan3 == "no"){
+//         document.write('You have lots to learn here!');
+//     }   else {
+//         coolOrNoWarn2();
+//     }
+// }
+
+let response = prompt('Question: Are the best movies from the 90s?');
+
+while (response.toLowerCase() != 'yes'){
+    response = prompt('wrong! Try again!');
 }
 
-function coolOrNoWarn(){
-    const movieFan2 = prompt("One more chance to be a good sport! Are the best movies from the 90s?")
-console.log(movieFan2);
+function rateMyPage(){
+    let rating = prompt("How many stars would you rate my page? 1-5");
 
-    if (movieFan2 == "yes"){
-        document.write('You are in the right place!');
-    }   else if (movieFan2 == "no"){
-        document.write('You have lots to learn here!');
-    }   else {
-        coolOrNoWarn2();
-    }
-}
-
-function coolOrNoWarn2(){
-    const movieFan3 = prompt("I can do this all day pal! Are the best movies from the 90s? Come on!")
-console.log(movieFan3);
-
-    if (movieFan3 == "yes"){
-        document.write('Finally, we agree on something!');
-    }   else if (movieFan3 == "no"){
-        document.write('You have lots to learn here!');
-    }   else {
-        coolOrNoWarn2();
+    for (let i = 0; i < rating; i++){
+        document.write("<img class='loop-img' src='star.png' alt='5 pointed yellow star in the style of a cartoon' />");
     }
 }
 
